4f09692fd90d895247978b9f28380a4655b6726d
K4traik <ericeliasquintana@gmail.com>
2026-09-18 12:05:42 -0300

    dao and daoimpl early work

--- powerDown/src/main/java/dao/SessionProgressionDao.java
+++ powerDown/src/main/java/dao/SessionProgressionDao.java
@@ -1,5 +1,10 @@
 package dao;
 
-public interface SessionProgressionDao {
+import model.SessionProgression;
 
+public interface SessionProgressionDao {
+	public void create(SessionProgression s);
+	public SessionProgression read(int id);
+	public void update(SessionProgression s);
+	public void delete(int id);
 }
--- powerDown/src/main/java/daoimpl/SessionProgressionDaoImpl.java
+++ powerDown/src/main/java/daoimpl/SessionProgressionDaoImpl.java
@@ -1,5 +1,23 @@
 package daoimpl;
 
-public class SessionProgressionDaoImpl {
+import dao.SessionProgressionDao;
+import model.SessionProgression;
 
+public class SessionProgressionDaoImpl implements SessionProgressionDao{
+	@Override
+	public void create(SessionProgression s) {
+		
+	}
+	@Override
+	public SessionProgression read(int id) {
+		return null;
+	}
+	@Override
+	public void update(SessionProgression s) {
+		
+	}
+	@Override
+	public void delete(int id) {
+		
+	}
 }
